0ebb468432eb4391909390a81e8afd64409e149e
TienN <tienn@tmtco.asia>
2023-01-20 11:16:08 +0700

    Add Areas Pages, config watch run on VS

---
+++ WebRazor/Areas/Product/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebRazor.Areas.Product
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
