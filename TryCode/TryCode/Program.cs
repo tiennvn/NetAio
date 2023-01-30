496b212f5e7212046a929a4010c0a411f96d3fa8
TienN <tienn@tmtco.asia>
2023-01-30 15:50:33 +0700

    Add try delegate

--- TryCode/TryCode/Program.cs
+++ TryCode/TryCode/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            RunCode.Run();
+            TryDelegate.Run();
         }
     }
 }
---
+++ TryCode/TryCode/RunCode/TryDelegate.cs
@@ -0,0 +1,53 @@
+﻿namespace TryCode
+{
+    internal class TryDelegate
+    {
+        protected TryDelegate()
+        {
+        }
+
+        public static void Run()
+        {
+            Console.WriteLine("RunCode");
+
+            Predicate<string> checkCase = IsUpperCase;
+            checkCase += IsLowerCase;
+
+            checkCase += (str) =>
+            {
+                bool result = str.Equals("T");
+                Console.WriteLine(result);
+                return result;
+            };
+
+            //bool result = checkCase("hello world!!");
+
+            //Console.WriteLine(result);
+
+            TestDelegate(checkCase, "T");
+        }
+
+        static bool IsUpperCase(string str)
+        {
+            bool result = str.Equals(str.ToUpper());
+            Console.WriteLine(result);
+            return result;
+        }
+
+        public static bool IsLowerCase(string str)
+        {
+            bool result = str.Equals(str.ToLower()); ;
+            Console.WriteLine(result);
+            return result;
+
+        }
+
+        public static void TestDelegate(Predicate<string> param, string str)
+        {
+            var result = param(str);
+            //result.
+        }
+
+
+    }
+}
